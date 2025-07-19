48c79db5401ba230f3bfa4bee6fd895af4ff350f
Imad Labrini <imadlabrini16@gmail.com>
2025-07-19 12:38:20 +0100

    first commit

---
+++ src/main/java/com/afaaq/afaaqdash/AfaaqDashApplication.java
@@ -0,0 +1,13 @@
+package com.afaaq.afaaqdash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AfaaqDashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AfaaqDashApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/afaaq/afaaqdash/AfaaqDashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.afaaq.afaaqdash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AfaaqDashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
