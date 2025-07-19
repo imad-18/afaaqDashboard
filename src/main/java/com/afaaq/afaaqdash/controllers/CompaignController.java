66682ac8b9b6cfca693be780a6afacf7414150af
Imad Labrini <youremail@example.com>
2025-07-19 17:03:46 +0100

    Build compaign deletion api

--- src/main/java/com/afaaq/afaaqdash/controllers/CompaignController.java
+++ src/main/java/com/afaaq/afaaqdash/controllers/CompaignController.java
@@ -3,20 +3,34 @@ package com.afaaq.afaaqdash.controllers;
 import com.afaaq.afaaqdash.entities.Compaign;
 import com.afaaq.afaaqdash.services.CompaignService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 public class CompaignController {
     @Autowired
     private CompaignService compaignService;
 
-    //create compaign
+    //create compaign API
     @PostMapping("/saveCompaign")
     public Compaign createCompaign(@RequestBody Compaign compaign) {
         System.out.println("Received admin: " + compaign);
         return compaignService.saveCompaign(compaign);
     }
+
+    //Compaign deletion API
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Map<String, String>> deleteCampaignByName(@PathVariable int id) {
+        compaignService.deleteCompaignFct(id);
+        return ResponseEntity.ok().body(Map.of("message", "Campaign deleted successfully !"));
+    }
+    /*public Compaign deleteCompaign(@PathVariable int id) {
+        System.out.println("Received admin: " + id);
+        Compaign deletedCompaign = compaignService.deleteCompaignFct(id);
+        System.out.println("deleted successfully: " + id);
+        return deletedCompaign;
+    }*/
 }
--- src/main/java/com/afaaq/afaaqdash/services/CompaignService.java
+++ src/main/java/com/afaaq/afaaqdash/services/CompaignService.java
@@ -2,8 +2,11 @@ package com.afaaq.afaaqdash.services;
 
 import com.afaaq.afaaqdash.entities.Compaign;
 import com.afaaq.afaaqdash.repository.CompaignRepo;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class CompaignService {
@@ -14,4 +17,25 @@ public class CompaignService {
     public Compaign saveCompaign(Compaign compaign) {
         return compaignRepo.save(compaign);
     }
+
+    public Optional<Compaign> findCompaignById(int id){
+        return compaignRepo.findById(id);
+    }
+
+    public Compaign deleteCompaignFct(int id){
+        Compaign compaign = findCompaignById(id).get();
+        compaignRepo.delete(compaign);
+        return compaign;
+    }
+    /*public Compaign deleteCompaignFct(int id) {
+        Optional<Compaign> optionalCompaign = compaignRepo.findById(id);
+        if (optionalCompaign.isPresent()) {
+            Compaign compaign = optionalCompaign.get();
+            compaignRepo.delete(compaign);
+            return compaign;
+        } else {
+            throw new EntityNotFoundException("Compaign not found with id: " + id);
+        }
+    }*/
+
 }
